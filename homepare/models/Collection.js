842a6a94e35574d036fc33b7b12bc08c68ea3c56
Jordan Manus <94319835+jordan-manus@users.noreply.github.com>
2024-01-11 10:04:35 -0500

    commiting to resolve merge conflicts.

--- homepare/models/Collection.js
+++ homepare/models/Collection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const CollectionsSchema = new mongoose.Schema({
+    collection_name: String,
     userID: String,
     houseID: String,
     //query params ???
