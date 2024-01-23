b54aea3cb4c1b8e13df93737ca2bfdb9c51670a5
Jordan Manus <94319835+jordan-manus@users.noreply.github.com>
2024-01-23 11:36:49 -0500

    got the sentiment analysis working.

--- homepare/sentiment-analysis.js
+++ homepare/sentiment-analysis.js
@@ -1,21 +1,29 @@
 const OpenAI = require('openai')
-const apikey = process.env.OPENAI_API_KEY
-// const apikey = "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"
+require('dotenv').config()
 const axios = require('axios')
-const client = axios.create({
-    headers: { 'Authorization': `Bearer ${apikey}` }
-})
+const { json } = require('express')
+const client = new OpenAI()
+
+// async function main() {
+//     const completion = await client.chat.completions.create({
+//         messages: [{ role: "system", content: "You are a helpful assistant." }],
+//         model: "gpt-3.5-turbo",
+//     });
+
+//     console.log(completion.choices[0]);
+// }
+
+// main();
 
 
+let transcription = JSON.stringify("This pad thai is really good")
+// JSON.stringify(transcription)
 
-let transcription = "This pad thai is really good"
-
-
-function sentiment_analysis(transcription) {
-    response = client.chat.completions.create(
-        model = "gpt-4",
-        temperature = 0,
-        messages = [
+async function sentiment_analysis(transcription) {
+    response = await client.chat.completions.create({
+        model: "gpt-4",
+        temperature: 0,
+        messages: [
             {
                 "role": "system",
                 "content": "As an AI with expertise in language and emotion analysis, your task is to analyze the sentiment of the following text. Please consider the overall tone of the discussion, the emotion conveyed by the language used, and the context in which words and phrases are used. Indicate whether the sentiment is generally positive, negative, or neutral, and provide brief explanations for your analysis where possible."
@@ -25,8 +33,9 @@ function sentiment_analysis(transcription) {
                 "content": transcription
             }
         ]
-    )
+    })
     return response['choices'][0]['message']['content']
 }
 
-sentiment_analysis(transcription);
+sentiment_analysis(transcription)
+    .then((results) => console.log(results))
